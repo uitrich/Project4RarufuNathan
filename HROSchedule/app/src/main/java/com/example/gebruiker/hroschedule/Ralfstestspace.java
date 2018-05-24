98b15788ddc756b5c97eb5203d5218b8f71069bd
RalfvTrigt <0944735@hr.nl>
2018-05-24 22:27:27 +0200

    Begin aan zooming functie

--- HROSchedule/app/src/main/java/com/example/gebruiker/hroschedule/Ralfstestspace.java
+++ HROSchedule/app/src/main/java/com/example/gebruiker/hroschedule/Ralfstestspace.java
@@ -6,8 +6,47 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.view.MotionEvent;
+
 
 public class Ralfstestspace extends AppCompatActivity {
+    private ImageView mImgTest;
+    private FrameLayout mPhotobox;
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+
+    //three states voor detection
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    //variabelen voor zooming
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1F;
+
+    private void bindViews(View rootView){
+        mPhotobox = (FrameLayout) rootView.findViewById(R.id.fotobox);
+        mImgTest = (ImageView) rootView.findViewById(R.id.imgTest);
+        mImgTest.setOnTouchListener(this);
+    }
+
+    private float spacing(MotionEvent event){
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0)- event.getY(1);
+        return (float) Math.sqrt(x*x + y*y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event){
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getX(1);
+        point.set(x / 2, y / 2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +64,45 @@ public class Ralfstestspace extends AppCompatActivity {
             }
         });
     }
+    public boolean onTouch(View v, MotionEvent event){
+        ImageView view = (ImageView) v;
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK){
+            case MotionEvent.ACTION_DOWN:
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f){
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG){
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+                }
+                else if (mode == ZOOM){
+                    float newDist = spacing(event);
+                    if (newDist > 10f){
+                        matrix.set(savedMatrix);
+                        float scale = newDist / oldDist;
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+        view.setImageMatrix(matrix);
+        return true;
+    }
+
 
 }
